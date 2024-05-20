d0d936c4bafb2173f9d75b005187dbde67bb0d80
Morgane-Compa <morgane.compafloury@gmail.com>
2024-05-20 19:01:43 +0200

    [feature/backgroundElements] : upgrade responsive

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
 import { ProfilePageComponent } from './pages/profile-page/profile-page.component';
 import { FeedPageComponent } from './pages/feed-page/feed-page.component';
-import { ConfidentialPoliticComponent } from './pages/confidential-politic/confidential-politic.component';
-import { CookiesPoliticComponent } from './pages/cookies-politic/cookies-politic.component';
-import { UseConditionsComponent } from './pages/use-conditions/use-conditions.component';
-import { ContactComponent } from './pages/contact/contact.component';
-import { UnsubscribeConditionsComponent } from './pages/unsubscribe-conditions/unsubscribe-conditions.component';
+
 
 const routes: Routes = [
   {
@@ -17,26 +13,6 @@ const routes: Routes = [
   {
     path: "profile/:id",
     component: ProfilePageComponent
-  },
-  {
-    path: "confidential-politic",
-    component: ConfidentialPoliticComponent
-  },
-  {
-    path: "cookies-politic",
-    component: CookiesPoliticComponent
-  },
-  {
-    path: "use-condition",
-    component: UseConditionsComponent
-  },
-  {
-    path: "unsubscribe",
-    component: UnsubscribeConditionsComponent
-  },
-  {
-    path: "contact",
-    component: ContactComponent
   },
   {
     path:"not-found",
