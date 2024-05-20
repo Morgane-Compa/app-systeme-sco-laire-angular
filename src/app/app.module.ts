bbf35c1f8df7e2a1c3898abe562e28247cb6bc71
Morgane-Compa <morgane.compafloury@gmail.com>
2024-05-20 15:18:45 +0200

    [feature/architecture] : addingcomponents and pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { FeedPageComponent } from './pages/feed-page/feed-page.component';
+import { ProfilePageComponent } from './pages/profile-page/profile-page.component';
+import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
+import { WhoWeAreComponent } from './pages/who-we-are/who-we-are.component';
+import { AddingModalComponentComponent } from './components/adding-modal-component/adding-modal-component.component';
+import { PostComponentComponent } from './components/post-component/post-component.component';
+import { HeaderComponentComponent } from './components/header-component/header-component.component';
+import { ActionButtonComponent } from './components/action-button/action-button.component';
+import { ProfileCardComponentComponent } from './components/profile-card-component/profile-card-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    FeedPageComponent,
+    ProfilePageComponent,
+    NotFoundPageComponent,
+    WhoWeAreComponent,
+    AddingModalComponentComponent,
+    PostComponentComponent,
+    HeaderComponentComponent,
+    ActionButtonComponent,
+    ProfileCardComponentComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/adding-modal-component/adding-modal-component.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-adding-modal-component',
+  templateUrl: './adding-modal-component.component.html',
+  styleUrls: ['./adding-modal-component.component.scss']
+})
+export class AddingModalComponentComponent {
+
+}
