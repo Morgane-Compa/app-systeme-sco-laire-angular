95ff6a359d936d419b6a1217e19cb8ef0be21715
Morgane-Compa <morgane.compafloury@gmail.com>
2024-06-16 14:00:46 +0200

    [feature/createAcount] : register is fonctional

--- src/app/pages/register-page/register-page.component.ts
+++ src/app/pages/register-page/register-page.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { School } from 'src/app/models/school';
+import { SchoolService } from 'src/app/services/school/school.service';
 import { UserService } from 'src/app/services/user/user.service';
 
 @Component({
@@ -9,18 +12,40 @@ import { UserService } from 'src/app/services/user/user.service';
 })
 export class RegisterPageComponent {
   registerForm: FormGroup;
-  constructor(private fb: FormBuilder, private userService: UserService) {
+  schools: School[] = [];
+
+  constructor(private fb: FormBuilder, private userService: UserService, private schoolService: SchoolService, private router: Router) {
     this.registerForm = this.fb.group({
-      username: ['', Validators.required],
+      firstname: ['', Validators.required],
+      lastname: ['', Validators.required],
+      mail: ['', [Validators.required, Validators.email]],
+      phone_number: ['', Validators.required],
       password: ['', Validators.required],
-      email: ['', [Validators.required, Validators.email]]
+      user_role: ['', Validators.required],
+      school_id: ['', Validators.required]
     });
-  };
+  }
+
+  ngOnInit(): void {
+    this.loadSchools();
+  }
+
+  loadSchools() {
+    this.schoolService.getAllSchools().subscribe(data => {
+      this.schools = data;
+    }, error => {
+      console.error('Failed to load schools', error);
+    });
+  }
 
   register() {
     if (this.registerForm.valid) {
-      this.userService.register(this.registerForm.value).subscribe(response => {
+      const formData = this.registerForm.value;
+      formData.user_image_id = 1;
+      
+      this.userService.register(formData).subscribe(response => {
         console.log('Registration successful', response);
+        this.router.navigate(['/login']);
       }, error => {
         console.error('Registration failed', error);
       });
--- src/app/shared/urls.ts
+++ src/app/shared/urls.ts
@@ -1,6 +1,6 @@
 const BASE_URL = "http://localhost:8080";
 
-export const SCHOOL = BASE_URL + "/api/schools";
+export const SCHOOL_URL = BASE_URL + "/api/schools";
 export const CLASSROOM = BASE_URL + "/api/classrooms";
 export const NEWS_URL = BASE_URL + "/api/news";
 export const PRIVATE_MESSAGES = BASE_URL + "/api/private_message";
